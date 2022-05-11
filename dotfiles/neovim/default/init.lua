b39e3f328482d231f8ed4d69d73a1d9abea36e42
Perseo <perseo.gi98@gmail.com>
2022-05-11 10:09:11 +0200

    added init.lua

---
+++ dotfiles/neovim/default/init.lua
@@ -0,0 +1,19 @@
+function CreateNoremapGlobal(type, opts)
+	return function(lhs, rhs)
+		vim.api.nvim_set_keymap(type, lhs, rhs, opts)
+	end
+end
+
+function CreateNoremap(type, opts)
+	return function(lhs, rhs, bufnr)
+        bufnr = bufnr or 0
+		vim.api.nvim_buf_set_keymap(bufnr, type, lhs, rhs, opts)
+	end
+end
+
+
+Nnoremap = CreateNoremap("n", { noremap = true })
+Inoremap = CreateNoremap("i", { noremap = true })
+
+
+require("plugins").setup()
