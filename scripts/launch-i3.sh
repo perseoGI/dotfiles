df7ec7ff5d43654e21273523f712e87988c81579
Perseo <perseo.gutierrez@seddi.com>
2022-09-22 11:12:04 +0200

    updated scripts

--- scripts/launch-i3.sh
+++ scripts/launch-i3.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f3 | cut -d"+" -f1); do
-    polybar --reload $m &
+    polybar --reload $m --config=~/.config/polybar/config.ini &
   done
 else
-  polybar --reload  &
+  polybar --reload --config=~/.config/polybar/config.ini &
 fi
