70b8c5ec86b9d89819a577457da4ca4b7f351baf
PerseoGI <perseog@jfrog.com>
2025-04-15 11:09:13 +0200

    Added basic sketchybar and aerospace config

--- dotfiles/neovim/nvchad/lua/configs/lspconfig.lua
+++ dotfiles/neovim/nvchad/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "bashls", "clangd", "pyright", "rust_analyzer", "yamlls", "ts_ls", "neocmake" }
+local servers = { "html", "cssls", "bashls", "clangd", "pyright", "rust_analyzer", "yamlls", "ts_ls", "neocmake", "rnix" }
 
 -- bash-language-server
 -- clangd
